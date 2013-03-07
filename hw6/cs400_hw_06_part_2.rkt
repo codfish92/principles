2d172ee78d2fc729db567d9ace3f612bdeeec551
codfish92 <akoellin@mines.edu>
2013-03-06 21:40:24 -0700

    finished part2

--- hw6/cs400_hw_06_part_2.rkt
+++ hw6/cs400_hw_06_part_2.rkt
@@ -1,3 +1,8 @@
+#lang racket
+;callaborated with Aakash shah, Maria deslis, Anastesa, Miki
+
+(require racket/function)
+(require racket/string)
 (define articles '(
      ((Test-Driven Learning: Intrinsic Integration of Testing into the CS/SE Curriculum)
       ((David Jansen)(Hossein Saiedian))
@@ -22,26 +27,68 @@
       ("Design Patterns" "Game of Life" "CS1" "Laboratory"))
    ))
 
-(define (getTitle articles)
-  (if (empty? articles)
-      (display "no more articles")
-      (begin (display (caar articles)) (display "\n") (getTitle (cdr articles)))
+(define (getTitle article)
+  (if (empty? article)
+      (display "not an article")
+      (car article)
       );end if
   );end define
 
 (define (getAuthors articles)
   (if (empty? articles)
-      (display "not a book")
-      (begin (display (car articles)) (display "\n") (getAuthors (cdr articles)))
+      (display "not an article")
+      (second articles)
       );end if
   );end define
 
-(define (keywordSearch word) 
-  #t
+(define (getKeyword article)
+  (if (empty? article)
+      (display "not an article")
+      (third article)
+      )
   );end define
-   
-   
-  
+;   
+;(define (createSearch keyword)
+ ; (lambda (x) (x keyword))
+ ; );end define
 
-(getTitle articles)
-(getAuthors articles)
+;(define (isInString x keywords)
+ ; (if (empty? keywords)
+ ;     #t
+ ;     #f
+ ;     );end if
+ ; );end define
+
+;communicated heavily with the collabartes up at the top of the page
+
+(define (keywordSearch keyword articles)
+  (map getTitle ( searchFunction (makeSearch keyword) articles)); apply getTitle to all matched articles from the doSearch function
+  );end define
+
+(define (makeSearch keyword)
+  (curry doSearch keyword); curry the keyword up so foldl can use it with 
+  );end define
+
+(define (searchFunction searchCurried articles) ;the curried function should be applied to all articles
+  (foldl searchCurried '() articles); use foldl with an empty list as the start state and articles on what to iterate over
+  );end define
+
+(define (doSearch keyword currentArticle matchedArticles)
+  (if (member keyword (getKeyword currentArticle)); if the keyword is in the the keywords?
+      (append matchedArticles (list currentArticle)); add the article to the list of matched articles
+      matchedArticles;otherwise just return the current matched articles
+      );end if
+  );end define 
+(keywordSearch "Java" articles)
+  
+;(define titleList '())
+;(define (old-keywordSearch keyword article titles)
+;  (if (empty? article)
+ ;     titles
+  ;    (if (list? (memq keyword (getKeyword (car article))))
+  ;        (keywordSearch keyword (cdr article) (append titles (getTitle (car article))))
+  ;        (keywordSearch keyword (cdr article) titles)
+  ;        );end if
+  ;    );end if
+  ;);end define
+;(string-ci=)
